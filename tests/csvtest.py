87dbae2ce198aec0c1f2681a2f9bcc1a3a5872a3
Nishida Yuto <yuto-ni@ahcclcsa01.naist.jp>
2022-12-04 22:04:37 +0900

    modify csvsample

--- tests/csvtest.py
+++ tests/csvtest.py
@@ -1,15 +1,15 @@
 import unittest
-from csvsample import CSVPrinter
+from SpecialLectureIS1.speciallectureis.csvsample import CSVPrinter
 
 
 class TestCSVPrinter(unittest.TestCase):
     def test_method1(self):
-        printer = CSVPrinter("sample.csv")
+        printer = CSVPrinter("../sample.csv")
         lines = printer.read()
         self.assertEqual(3, len(lines))
 
     def test_method2(self):
-        printer = CSVPrinter("sample.csv")
+        printer = CSVPrinter("../sample.csv")
         lines = printer.read()
         self.assertEqual("value2B", lines[1][1])
 
