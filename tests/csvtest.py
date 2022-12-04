8890de8dad779a4b3ce47227f9d5009c4818da62
Nishida Yuto <yuto-ni@ahcclcsa01.naist.jp>
2022-12-04 22:32:29 +0900

    modify tests

--- tests/csvtest.py
+++ tests/csvtest.py
@@ -14,6 +14,11 @@ class TestCSVPrinter(unittest.TestCase):
         self.assertEqual("value2B", lines[1][1])
 
     def test_method3(self):
+        printer = CSVPrinter("sample.tsv", tsv=True)
+        lines = printer.read()
+        self.assertEqual("value2B", lines[1][1])
+
+    def test_method4(self):
         with self.assertRaises(FileNotFoundError):
             printer = CSVPrinter("not_exist.csv")
             printer.read()
